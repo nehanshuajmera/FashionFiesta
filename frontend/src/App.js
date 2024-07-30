592585c75bf86720c58d21db53983e6643850e4d
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-07-30 20:33:23 +0530

    Shop page, collection preview and collection item component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/homepage/homepage.component';
+import { ShopPage } from './pages/shop/shop.component';
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
       </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
