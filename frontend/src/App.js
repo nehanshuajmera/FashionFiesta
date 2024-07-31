4404903eeeeb91b81be28d7548ec68d0d38d2de9
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-07-31 21:22:24 +0530

    Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/homepage/homepage.component';
 import { ShopPage } from './pages/shop/shop.component';
+import { Header } from './universal/header/header.component';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
---
+++ frontend/src/universal/header/header.component.jsx
@@ -0,0 +1,18 @@
+import { Link, NavLink } from 'react-router-dom';
+import './header.styles.scss';
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <Link to="/">
+          <img src="Logo.png" alt="Logo" />
+        </Link>
+      </div>
+      <div className="options">
+        <NavLink to="/shop" className="option">Shop</NavLink>
+        <NavLink to="/contact" className="option">Contact</NavLink>
+      </div>
+    </div>
+  )
+}
