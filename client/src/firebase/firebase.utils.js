ff16e79f2220a7b536537fb4888a8c9ca4fd86ea
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2024-09-12 23:39:06 +0530

    Security Bug fixes on frontend

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -10,12 +10,12 @@ import {
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore"; // For Firestore database (optional)
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwQkehzHaqoQn8CEDAo63CIUOmJ7qjp_0",
-  authDomain: "fashion-fiesta-f3afa.firebaseapp.com",
-  projectId: "fashion-fiesta-f3afa",
-  storageBucket: "fashion-fiesta-f3afa.appspot.com",
-  messagingSenderId: "170766499606",
-  appId: "1:170766499606:web:faa2fcba0b27fd26d09a37",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
